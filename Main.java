20fc7ca74cd597cbe361d37423484e315b2afc45
Joel Celinder <joelcel@kth.se>
2026-02-15 01:26:09 +0100

    Main 1.1

--- Main.java
+++ Main.java
@@ -7,77 +7,75 @@ public class Main {
     }
 
     static Direction curDir = Direction.NORTH;
-    static int northCars = 1, southCars = 1, TRIPS = 2, NC = 0, SC = 0, NW = 0, SW = 0;
-    static long crossingDelay = 1_000_000_000L, UTurnDelay = 2_000_000_000L;
+    static int northCars = 4, southCars = 4, TRIPS = 4, 
+    NC = 0, SC = 0, NW = 0, SW = 0, 
+    crossingDelay = 1000, UTurnDelay = 2000;
     static Object gate = new Object();
 
-    public static void bridge (int id, Direction dir) throws InterruptedException{
-        long startTime, curTime;
+    public static void bridge (int id, Direction dir) throws InterruptedException {
         int trips = TRIPS;
         if (dir == Direction.NORTH) {
-            System.out.printf("Car-%d started driving north at %s%n", id, LocalTime.now());
+            System.out.printf("Car:%d started driving north at %s%n", id, LocalTime.now());
         }
         else {
-            System.out.printf("Car-%d started driving south at %s%n", id, LocalTime.now());
+            System.out.printf("Car:%d started driving south at %s%n", id, LocalTime.now());
         }
 
         while (trips > 0) {
-            synchronized(gate) {
-                if (dir == Direction.NORTH) {
+            if (dir == Direction.NORTH) {
+                synchronized(gate) {
                     NW++;
-                    System.out.printf("Car-%d is waiting to enter the bridge from the north side at %s%n", id, LocalTime.now());
+                    System.out.printf("Car:%d is waiting to enter the bridge from the south side at %s%n", id, LocalTime.now());
                     while (dir != curDir) {
                         gate.wait();
                     }
                     NW--;
-                    System.out.printf("Car-%d started to cross the bridge from the north side at %s%n", id, LocalTime.now());
+                    System.out.printf("Car:%d started to cross the bridge from the south side at %s%n", id, LocalTime.now());
                     NC++;
-                    startTime = System.nanoTime();
-                    while (((curTime = System.nanoTime()) - startTime) < crossingDelay) {
-                        gate.wait(100);
-                    }
+                }
+                Thread.sleep(crossingDelay);
+
+                synchronized(gate) {
                     NC--;
-                    System.out.printf("Car-%d left the bridge at %s%n%n", id, LocalTime.now());
+                    System.out.printf("Car:%d left the bridge with %d trips left at %s%n", id, trips - 1, LocalTime.now());
                     dir = Direction.SOUTH;
-                    if ((NC == 0 && SW > NW) && SW > 0) {
+                    if (NC == 0) {
                         curDir = Direction.SOUTH;
+                        System.out.printf("%n");
                     }
                     gate.notifyAll();
                     trips--;
-                    startTime = System.nanoTime();
-                    while (((curTime = System.nanoTime()) - startTime) < UTurnDelay) {
-                        gate.wait(100);
-                    }
                 }
-                else if (dir == Direction.SOUTH) {
+                Thread.sleep(UTurnDelay);
+            }
+            else if (dir == Direction.SOUTH) {
+                synchronized(gate) {
                     SW++;
-                    System.out.printf("Car-%d is waiting to enter the bridge from the south side at %s%n", id, LocalTime.now());
+                    System.out.printf("Car:%d is waiting to enter the bridge from the north side at %s%n", id, LocalTime.now());
                     while (dir != curDir) {
                         gate.wait();
                     }
                     SW--;
-                    System.out.printf("Car-%d started to cross the bridge from the south side at %s%n", id, LocalTime.now());
+                    System.out.printf("Car:%d started to cross the bridge from the north side at %s%n", id, LocalTime.now());
                     SC++;
-                    startTime = System.nanoTime();
-                    while (((curTime = System.nanoTime()) - startTime) < crossingDelay) {
-                        gate.wait(100);
-                    }
+                }
+                Thread.sleep(crossingDelay);
+                
+                synchronized(gate) {
                     SC--;
-                    System.out.printf("Car-%d left the bridge at %s%n%n", id, LocalTime.now());
+                    System.out.printf("Car:%d left the bridge with %d trips left at %s%n", id, trips - 1, LocalTime.now());
                     dir = Direction.NORTH;
-                    if ((SC == 0 && NW > SW) && NW > 0) {
+                    if (SC == 0) {
                         curDir = Direction.NORTH;
+                        System.out.printf("%n");
                     }
                     gate.notifyAll();
                     trips--;
-                    startTime = System.nanoTime();
-                    while (((curTime = System.nanoTime()) - startTime) < UTurnDelay) {
-                        gate.wait(100);
-                    }
                 }
+                Thread.sleep(UTurnDelay);
             }
         }
-    } // Obama
+    }
     
     public static void main (String[] args) {
         if (args.length == 3) {
